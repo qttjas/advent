52b984fe364880b91abeae7740e6bb95823496b1
jas <122589193+qttjas@users.noreply.github.com>
2024-12-04 13:18:44 -0500

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,11 +1,36 @@
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 
-class Main {
-    public static void main(String[] args) {
-        String numbers = "2   3" +
-                         "3   4" +
-                         "2   5";
-        String[] result = numbers.split("   ");
-        System.out.println("result = " + Arrays.toString(result));
+public class Main {
+    public static void main(String args[]) throws IOException {
+        String fileName = new String(Files.readAllBytes(Paths.get("file.txt")));
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        Scanner scanner = new Scanner(fileName);
+
+        while (scanner.hasNextLine()) {
+            String[] parts = scanner.nextLine().split("   ");
+            left.add(Integer.parseInt(parts[0]));
+            right.add(Integer.parseInt(parts[1]));
+        }
+
+        scanner.close();
+        Collections.sort(left);
+        Collections.sort(right);
+
+    int totalDistance = 0;
+    for (int i = 0; i<left.size(); i++) {
+        totalDistance += Math.abs(left.set(i, 2)- right.set(i, 2));
+        }
+
+    System.out.println(totalDistance);
     }
 }
