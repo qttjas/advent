abe9cac7fb6a4a6072b58d9f846d5eca3ddf1768
jas <122589193+qttjas@users.noreply.github.com>
2024-12-06 12:59:08 -0500

    Update dayOne.java

--- dayOne.java
+++ dayOne.java
@@ -5,14 +5,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class dayOne {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(new File("src/numbers.txt"));
 
-        ArrayList<Integer> left = new ArrayList<Integer>();
-        ArrayList<Integer> right = new ArrayList<Integer>();
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
-            String[] parts = scanner.nextLine().split("   ");
+            String[] parts = scanner.nextLine().split(" {3}");
             left.add(Integer.parseInt(parts[0]));
             right.add(Integer.parseInt(parts[1]));
         }
