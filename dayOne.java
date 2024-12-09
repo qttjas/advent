31b453b1f6bb2e88d8d344a0846a6613d47ad313
jas <122589193+qttjas@users.noreply.github.com>
2024-12-09 12:18:55 -0500

    Update dayOne.java

--- dayOne.java
+++ dayOne.java
@@ -5,14 +5,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class dayOne {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         Scanner scanner = new Scanner(new File("src/numbers.txt"));
 
-        ArrayList<Integer> left = new ArrayList<>();
-        ArrayList<Integer> right = new ArrayList<>();
+        ArrayList<Integer> left = new ArrayList<Integer>();
+        ArrayList<Integer> right = new ArrayList<Integer>();
 
         while (scanner.hasNextLine()) {
-            String[] parts = scanner.nextLine().split(" {3}");
+            String[] parts = scanner.nextLine().split("   ");
             left.add(Integer.parseInt(parts[0]));
             right.add(Integer.parseInt(parts[1]));
         }
@@ -26,5 +26,18 @@ public class dayOne {
         }
 
         System.out.println(totalDistance);
+
+        int score = 0;
+        for (int numLeft : left) {
+            int count = 0;
+            
+            for (int numRight : right) {
+                if (numLeft == numRight) {
+                    count++;
+                }
+            }
+            score += numLeft * count;
+        }
+        System.out.println(score);
     }
 }
