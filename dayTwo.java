499bf9248d885b7e62b8f6c1aa45a265eeef5f74
jas <122589193+qttjas@users.noreply.github.com>
2024-12-11 12:23:49 -0500

    fixed errors

--- dayTwo.java
+++ dayTwo.java
@@ -5,18 +5,41 @@ import java.util.Scanner;
 public class dayTwo {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(new File("src/numbersTwo.txt"));
-        String[] reports = scanner.nextLine().split("\n");
         int count = 0;
 
-        for (String report: reports) {
+        while (scanner.hasNextLine()) {
+            String report = scanner.nextLine();
             if (isSafe(report)) {
                 count++;
             }
-            System.out.println(count);
         }
+
+        System.out.println(count);
+        scanner.close();
     }
 
     private static boolean isSafe(String report) {
+        String[] levels = report.split(" ");
+        int[] nums = new int[levels.length];
 
+        for (int i = 0; i < levels.length; i++) {
+            nums[i] = Integer.parseInt(levels[i]);
+        }
+
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+                return false;
+            }
+            if (diff > 0) {
+                decreasing = false;
+            } else if (diff < 0) {
+                increasing = false;
+            }
+        }
+        return increasing || decreasing;
     }
 }
