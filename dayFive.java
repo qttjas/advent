1fe24838147e82655d8d6b0221a8ef8610bebf90
jas <122589193+qttjas@users.noreply.github.com>
2024-12-12 12:55:01 -0500

    Add files via upload

---
+++ dayFive.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class dayFive {
+    public static void main(String args[]) throws IOException {
+        Scanner scanner = new Scanner(new File("src/numbersFive.txt"));
+
+
+
+
+    }
+}
