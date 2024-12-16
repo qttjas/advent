44276805cd63094a7b2f8454f2e7b6abaed8c125
jas <122589193+qttjas@users.noreply.github.com>
2024-12-16 13:17:35 -0500

    Add files via upload

---
+++ daySeven.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class daySeven {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(new File("src/numbersSeven.txt"));
+        List<int[]> nums= new ArrayList<>();
+        List<int[]> test= new ArrayList<>();
+
+
+
+        }
+}
